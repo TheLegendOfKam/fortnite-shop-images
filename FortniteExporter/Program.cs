5ee343b9df7d0a62725361f32d5f8caaf631c57b
TheLegendOfKam <141887820+TheLegendOfKam@users.noreply.github.com>
2025-10-03 15:56:20 -0700

    Update Program.cs

--- FortniteExporter/Program.cs
+++ FortniteExporter/Program.cs
@@ -1,35 +1,28 @@
 using System;
+using System.IO;
 using CUE4Parse.FileProvider;
 using CUE4Parse.UE4.Assets.Exports.Texture;
-using System.IO;
 
-class Program
-{
-    static void Main()
-    {
-        string pakDir = "./paks";   // Fortnite pak files go here (next step we’ll automate download)
+class Program {
+    static void Main() {
+        string pakDir = "./paks";
         string outDir = "./docs";
 
         var provider = new DefaultFileProvider(pakDir, SearchOption.AllDirectories);
         provider.Initialize();
 
-        foreach (var file in provider.Files)
-        {
-            if (file.Value.Path.StartsWith("/OfferCatalog/Textures"))
-            {
-                try
-                {
+        foreach (var file in provider.Files) {
+            if (file.Value.Path.Contains("/OfferCatalog/Textures/")) {
+                try {
                     var obj = provider.LoadObject(file.Value.Path);
-                    if (obj is UTexture2D tex)
-                    {
-                        var png = tex.Decode();
+                    if (obj is UTexture2D tex) {
+                        var data = tex.Decode();
                         var savePath = Path.Combine(outDir, file.Value.Path.TrimStart('/') + ".png");
-                        Directory.CreateDirectory(Path.GetDirectoryName(savePath));
-                        File.WriteAllBytes(savePath, png);
-                        Console.WriteLine("Exported " + savePath);
+                        Directory.CreateDirectory(Path.GetDirectoryName(savePath)!);
+                        File.WriteAllBytes(savePath, data);
+                        Console.WriteLine($"Exported {savePath}");
                     }
-                }
-                catch { }
+                } catch { }
             }
         }
     }
